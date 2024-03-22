2e8c1b309aa9b7b05d2df5fa1c2eb73aebd84a23
Борис Кебаб <levon44432@gmail.com>
2024-03-22 22:29:02 +0300

    added controllers for user and order

--- FiestaMarketBackend.API/Controllers/OrderController.cs
+++ FiestaMarketBackend.API/Controllers/OrderController.cs
@@ -1,6 +1,69 @@
-﻿namespace FiestaMarketBackend.API.Controllers
+﻿using FiestaMarketBackend.Application.Order.Commands;
+using FiestaMarketBackend.Application.Order.Queries;
+using FiestaMarketBackend.Application.User.Commands;
+using FiestaMarketBackend.Application.User.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace FiestaMarketBackend.API.Controllers
 {
-    public class OrderController
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var query = new GetOrderByIdQuery { Id = id };
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Get(GetOrdersQuery query)
+        {
+            await _mediator.Send(query);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpPatch]
+        [Route("[action]")]
+        public async Task<IActionResult> Update(UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("[action]")]
+        public async Task<IActionResult> Delete(DeleteOrderCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("[action]")]
+        public async Task<IActionResult> DeleteFromFavorite(CreateOrderCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
     }
 }
--- FiestaMarketBackend.API/Controllers/UserController.cs
+++ FiestaMarketBackend.API/Controllers/UserController.cs
@@ -1,6 +1,146 @@
-﻿namespace FiestaMarketBackend.API.Controllers
+﻿using FiestaMarketBackend.Application.Responses;
+using FiestaMarketBackend.Application.User.Commands;
+using FiestaMarketBackend.Application.User.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FiestaMarketBackend.API.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UserController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<List<UserResponse>>> GetAllUsers(GetAllUsersQuery query)
+        {
+            await _mediator.Send(query);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<UserResponse>> GetById(GetByIdQuery query)
+        {
+            await _mediator.Send(query);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<CartResponse>> GetCart(GetCartQuery query)
+        {
+            await _mediator.Send(query);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<FavoriteResponse>> GetFavorites(GetFavoritesQuery query)
+        {
+            await _mediator.Send(query);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult<List<OrderResponse>>> GetOrders(GetUserOrdersQuery query)
+        {
+            await _mediator.Send(query);
+            var result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> CreateUser(CreateUserCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> AddToCart(AddToCartCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> AddToFavorite(AddToFavoriteCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("[action]")]
+        public async Task<IActionResult> UpdateQtyInCart(UpdateQtyInCartCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("[action]")]
+        public async Task<IActionResult> DeleteFromCart(DeleteFromCartCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("[action]")]
+        public async Task<IActionResult> DeleteFromFavorite(DeleteFromFavoriteCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> UpdateUser(UpdateUserCommand command)
+        {
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+
+        [HttpDelete]
+        [Route("[action]")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var command = new DeleteUserCommand { Id = id};
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+
+
     }
 }
